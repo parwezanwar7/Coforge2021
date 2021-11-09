698e95218bf69336e858df03b843c73c0516907d
parwezanwar7 <72470359+parwezanwar7@users.noreply.github.com>
2021-11-09 12:34:55 +0530

    DI using constructor and setter based

---
+++ SpringConstructorBasedInjection/src/com/demo/Main.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	// use ctrl+space for suggestion of the word below
+	ApplicationContext	context = new ClassPathXmlApplicationContext("applicationContext.xml");
+	
+	Message mes = (Message)context.getBean("myconstructorapp");
+	
+	mes.show();
+	}
+
+}
