c24dd0bc92eabadda095ea471ee030abd03020f1
parwezanwar7 <72470359+parwezanwar7@users.noreply.github.com>
2021-11-09 12:21:07 +0530

    ioc concept

---
+++ SpringApplicationContextDemo/src/com/demo/Message.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class Message {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public void show() {
+
+		System.out.println(" from message " + message);
+	}
+
+}
