c53fea4a5b4af5d29cf8653807a320c92dc34bff
parwezanwar7 <72470359+parwezanwar7@users.noreply.github.com>
2021-11-09 12:59:18 +0530

    scopes in spring

---
+++ SpringBeanSingletonScope/src/com/demo/MainClient.java
@@ -0,0 +1,36 @@
+package com.demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainClient {
+	
+	
+	public static void main (String arg[]) {
+		
+		
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        //forA
+		Message messagserivceA = (Message) context.getBean("messageservice");
+		//for B
+		Message messagserivceB = (Message) context.getBean("messageservice");
+		
+		
+		///setting some values
+		messagserivceA.setMessage("Message of  A");
+		
+		System.out.println(" from A  -->        "+messagserivceA.getMessage());
+		System.out.println(" from A (object name) -->        "+messagserivceA);// object name start from @ like 17dec50e
+		
+		
+		
+		
+		System.out.println(" from B -->    "+messagserivceB.getMessage());
+		System.out.println(" from B (object name) -->        "+messagserivceB);
+		
+		
+		
+	}
+
+}
